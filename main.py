16fc2df96512e25e97fc518a0e74daf057d2d65f
pattarapongyaimuang <pattarapongyaimuang@gmail.com>
2025-06-15 11:02:02 +0700

    add method get full name

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # 👉 Code for Stage 5 goes here
 class Student:
-    def __init__(self, name, lastn, age, courses, pocket):
-        self.name = name
-        self.lastn = lastn
+    def __init__(self, fname, lname, age, courses, pocket):
+        self.fname = fname
+        self.lname = lname
         self.age = age
         self.courses = courses
         self.pocket = pocket
+    
+    def get_fullname(self):
+        return print(f"{self.fname} {self.lname}")
 
     def display_info(self):
-        print("Name:", self.name)
-        print("Last name:", self.lastn)
+        print("Name:", self.fname)
+        print("Last name:", self.lname)
         print("Age:", self.age)
         print("Courses:", ", ".join(self.courses))
         print("Pockey money:",self.pocket)
@@ -20,4 +23,6 @@ student2 = Student("Bob","Bomba", 22, ["Physics", "History"],200)
 
 student1.display_info()
 student2.display_info()
+student1.get_fullname()
+student2.get_fullname()
 # Feel free to modify or expand it
